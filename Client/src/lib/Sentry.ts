db69d98c4584f0ab6badedc3e700634b0f2ed298
Cosmosuperbaka <64939678+Cosmosuperbaka@users.noreply.github.com>
2026-09-12 00:21:47 +0800

    fix(Core):完善版本标识

--- Client/src/lib/Sentry.ts
+++ Client/src/lib/Sentry.ts
@@ -23,6 +23,7 @@ export const isClientSentryEnabled = (): boolean => isInitialized;
 
 export interface SentryOptions {
   dsn?: string;
+  release?: string;
   serverUrl?: string;
   tracesSampleRate?: number;
   replaysSessionSampleRate?: number;
@@ -49,7 +50,10 @@ export const initClientSentry = (
     dsn,
     tunnel,
     environment: import.meta.env.MODE,
-    release: commitHistory.currentCommit !== "dev" ? `bakagame-client@${commitHistory.currentCommit}` : undefined,
+    release:
+      options?.release ??
+      import.meta.env.VITE_SENTRY_RELEASE ??
+      (commitHistory.currentCommit !== "dev" ? `bakagame-client@${commitHistory.currentCommit}` : undefined),
     tracesSampleRate: options?.tracesSampleRate ?? 0.1,
     replaysSessionSampleRate: options?.replaysSessionSampleRate ?? 0.1,
     replaysOnErrorSampleRate: options?.replaysOnErrorSampleRate ?? 1,
